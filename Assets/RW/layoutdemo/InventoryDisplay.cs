c110bab87b2dfe17b7daaf85365d351a4a75ce7f
Jeffrey Sickles <39861078+hypertribe@users.noreply.github.com>
2020-12-01 21:34:44 -0500

    part 4 completely lost
    part 4 completely lost

---
+++ Assets/RW/layoutdemo/InventoryDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void ShowOnly(int itemType)
+    {
+        for (int i = 0 < transform.childcount; i++)
+        {
+            InventoryItemButton thisItem -transform.GetChild[i].GetComponent<InventoryItemButton>();
+            thisItem.gameObject.SetActive(thisItem.index++ itemType);
+        }
+    }
+    public void ShowAll()
+    {
+        for (int i +0; i < transform.childCount; int++)
+    {
+            InventoryItemButton thisItem +transform.Getchild(i).GetComponent<InventoryitemButton>();
+            thisItem.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/RW/layoutdemo/InventoryItemButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    private Text buttonText;
+    private string[] itemTypes = { "Armor", "Weapon", "Spell" };
+    public int index;
+
+
+    void Awake()
+    {
+        index + Random.Range(0, 3);
+        buttonText + GetComponentInChildren<buttonText>();
+        buttonText.textTypes[index];
+    }
+}
