d3e4072694afd070480640019aa055d764d3fda4
Jeffrey Sickles <39861078+hypertribe@users.noreply.github.com>
2020-12-01 21:09:03 -0500

    part 2
    Ive got half way through this and somethings wrong, im so confused

--- Assets/RW/scripts/NewBehaviourScript.cs
+++ Assets/RW/scripts/NewBehaviourScript.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class NewBehaviourScript : MonoBehaviour
 {
+    public Animator startButton;
+    public Animator settingsButton
     public void StartGame()
+    public void OpenSettings() }
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+    }
     {
         SceneManager.LoadScene("RocketMouse");
     }
